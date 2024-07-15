623ba81b92008a3d031d0333b64d31810f943193
Kartlos <42392797+99kartlos@users.noreply.github.com>
2024-07-16 02:19:32 +0300

    Update setup_light_validator.sh

--- setup_light_validator.sh
+++ setup_light_validator.sh
@@ -1,48 +1,47 @@
 #!/bin/bash
 
-# Download the light validator binary package
-echo "Downloading the light validator binary package..."
-curl -O https://dill-release.s3.ap-southeast-1.amazonaws.com/linux/dill.tar.gz
+# Define variables
+BINARY_URL_LINUX="https://dill-release.s3.ap-southeast-1.amazonaws.com/linux/dill.tar.gz"
+BINARY_URL_MACOS="https://dill-release.s3.ap-southeast-1.amazonaws.com/macos/dill.tar.gz"
+BINARY_URL=$BINARY_URL_LINUX
+DOWNLOAD_DIR="$HOME/dill"
+KEYS_DIR="$DOWNLOAD_DIR/validator_keys"
+KEYSTORE_DIR="$DOWNLOAD_DIR/keystore"
+PASSWORD_FILE="$DOWNLOAD_DIR/walletPw.txt"
+
+# Ask for OS type
+read -p "Are you using macOS? (yes/no): " os_type
+if [ "$os_type" == "yes" ]; then
+    BINARY_URL=$BINARY_URL_MACOS
+fi
+
+# Download the binary package
+echo "Downloading binary package..."
+curl -O $BINARY_URL
 
 # Extract the package
-echo "Extracting the package..."
+echo "Extracting package..."
 tar -xzvf dill.tar.gz && cd dill
 
 # Generate validator keys
 echo "Generating validator keys..."
 ./dill_validators_gen new-mnemonic --num_validators=1 --chain=andes --folder=./
 
-# Prompt user to create a password for the keystore
-echo "Please create a password for your validator keystore:"
-read -s -p "Enter password: " keystore_password
+# Import your keys to your keystore
+echo "Importing keys to keystore..."
+./dill-node accounts import --andes --wallet-dir $KEYSTORE_DIR --keys-dir $KEYS_DIR --accept-terms-of-use
+
+# Ask for wallet password
+read -sp "Enter your wallet password: " wallet_password
 echo
-read -s -p "Confirm password: " keystore_password_confirm
-echo
-
-# Check if passwords match
-if [ "$keystore_password" != "$keystore_password_confirm" ]; then
-  echo "Passwords do not match. Please run the script again and enter matching passwords."
-  exit 1
-fi
-
-# Save the password to a file
-echo "Saving the keystore password to a file..."
-echo "$keystore_password" > walletPw.txt
-
-# Import the keys to the keystore
-echo "Importing your keys to the keystore..."
-./dill-node accounts import --andes --wallet-dir ./keystore --keys-dir validator_keys/ --accept-terms-of-use
+echo $wallet_password > $PASSWORD_FILE
 
 # Start the light validator node
-echo "Starting the light validator node..."
-./start_light.sh -p walletPw.txt
+echo "Starting light validator node..."
+./start_light.sh -p $PASSWORD_FILE
 
-# Check if the node is running
+# Check if the node is up and running
 echo "Checking if the node is up and running..."
-ps -ef | grep dill | grep -v grep
+ps -ef | grep dill
 
-echo "Light validator setup complete!"
-
-# Ensure we are in the ~/dill directory
-cd ~/dill
-echo "You are now in the ~/dill directory."
+echo "Setup complete!"
